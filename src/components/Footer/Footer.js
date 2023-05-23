ee0ff8a8ba033ffcb9ba5ba84b63825514f9ca4a
Gabo <gabocanseco@gmail.com>
2023-05-23 02:10:12 -0500

    Footer done

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,27 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+      <LinkList>
+        <LinkColumn>
+          <LinkTitle>Call</LinkTitle>
+          <LinkItem href="tel:52+ 951-248-6590">52+ 951-248-6590</LinkItem>
+          <LinkTitle>Email</LinkTitle>
+          <LinkItem href="mailto:gabocanseco@gmail.com">gabocanseco@gmail.com</LinkItem>
+        </LinkColumn>
+      </LinkList>
+      <SocialIconsContainer>
+        <SocialContainer>
+          <SocialIcons href="https://github.com/Gab0CS" target='blank'>
+            <AiFillGithub size="3rem"/>
+          </SocialIcons>
+          <SocialIcons href="https://www.linkedin.com/in/gabriel-canseco" target='blank'>
+            <AiFillLinkedin size="3rem"/>
+          </SocialIcons>
+          </SocialContainer>
+      
+      </SocialIconsContainer>
+    </FooterWrapper>
   );
 };
 
