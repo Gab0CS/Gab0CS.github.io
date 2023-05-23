2648170322e4b13a8d019f21c6a3383e7b07933f
Gabo <gabocanseco@gmail.com>
2023-05-22 21:17:57 -0500

    Project section finished

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,19 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+        Welcome to <br />
+      My Portfolio
+      </SectionTitle>
+      <SectionText>
+        Hello, I'm a Front-end developer and in this Portfolio rest 
+        information about me and some of my projects. 
+      </SectionText>
+      <Button onClick={() => window.location = 'https://github.com/Gab0CS/Gab0CS.github.io'}> Portfolio repository</Button>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -3,11 +3,45 @@ import React from 'react';
 import { BlogCard, CardInfo, ExternalLinks, GridContainer, HeaderThree, Hr, Tag, TagList, TitleContent, UtilityList, Img } from './ProjectsStyles';
 import { Section, SectionDivider, SectionTitle } from '../../styles/GlobalComponents';
 import { projects } from '../../constants/constants';
-
+const projectsDemo = [{
+  title: 'Project 1',
+  description: 'Description about the project'
+},{
+  title: 'Project 2',
+  description: 'Description about the project'
+},{
+  title: 'Project 3',
+  description: 'Description about the project'
+}];
 const Projects = () => (
-  <div>
-    Projects
-  </div>
+  <Section nopadding id="projects">
+    <SectionDivider />
+    <SectionTitle main>Projects</SectionTitle>
+    <GridContainer>
+        {projects.map((project) => (
+          <BlogCard key={project.id}>
+            <Img src={project.image} />
+            <TitleContent>
+              <HeaderThree title>{project.title}</HeaderThree>
+              <Hr />
+            </TitleContent>
+            <CardInfo>{project.description}</CardInfo>
+            <div>
+              <TitleContent>Stack</TitleContent>
+              <TagList>
+                {project.tags.map((tag, i) =>(
+                  <Tag key={i}>{tag}</Tag>
+                ))}
+              </TagList>                  
+            </div>
+            <UtilityList>
+              <ExternalLinks href={project.visit} target='blank'>Githhub repository</ExternalLinks>
+              <ExternalLinks href={project.source} target='blank'>Source</ExternalLinks>
+            </UtilityList>
+          </BlogCard>
+          ))}
+    </GridContainer>
+  </Section>
 );
 
 export default Projects;
